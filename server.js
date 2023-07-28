b59e3eeb578aa46b428d9f9744a0fdb6ac2237ab
chauhankevin982@gmail.com <rwnkevinchauhan@gmail.com>
2023-07-28 14:48:03 +0530

    new: cart,auth page added

--- server.js
+++ server.js
@@ -9,15 +9,27 @@ const PORT = process.env.PORT || 3000
 // Assets
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-
-// set template engine
+// set template engine (its should be placed before routes)
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
+
+// routes
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/cart',(req,res)=>{
+    res.render('customer/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
+// listening server
 app.listen(PORT,()=>{
     console.log('listening on port',PORT)
 })
